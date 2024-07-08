a897874b5a234454b39def15761bf53973319725
Yash Singhal <singhaly914@gmail.com>
2024-07-08 18:15:21 +0530

    db connection

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+dotenv.config();
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
